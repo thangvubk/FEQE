2d2466d962c2026cad5dd7d3d8983294156063be
thangvu <thangvubk@gmail.com>
2018-09-01 10:48:43 +0900

    clean up

--- main.py
+++ main.py
@@ -11,45 +11,67 @@ from config import config, log_config
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 import argparse
+import pdb
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--checkpoint', type=str, default='checkpoint')
-parser.add_argument('--vgg_dir', type=str, default='vgg_pretrained/imagenet-vgg-verydeep-19.mat')
-parser.add_argument('--sample_type', type=str, default='subpixel')
-parser.add_argument('--conv_type', type=str, default='default')
-parser.add_argument('--body_type', type=str, default='resnet')
-parser.add_argument('--n_feats', type=int, default=16)
-parser.add_argument('--n_blocks', type=int, default=16)
-parser.add_argument('--n_groups', type=int, default=0)
-parser.add_argument('--n_convs', type=int, default=0)
-parser.add_argument('--n_squeezes', type=int, default=0)
-parser.add_argument('--scale', type=int, default=4)
-parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--eval_every', type=int, default=20)
-parser.add_argument('--pretrained_model', type=str, default='')
+# data
 parser.add_argument('--train_path', type=str, default='./data/DIV2K_train_HR')
 parser.add_argument('--valid_path', type=str, default='./data/DIV2K_valid_HR_9')
-parser.add_argument('--phase', type=str, default='train')
-parser.add_argument('--alpha_l1', type=float, default=1)
-parser.add_argument('--alpha_vgg', type=float, default=0)
+parser.add_argument('--scale', type=int, default=4,
+                    help='downsample scale')
+# Train ops
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--learning_rate', type=float, default=1e-4)
+parser.add_argument('--n_epochs', type=int, default=2000)
+parser.add_argument('--decay_every', type=int, default=1200,
+                    help='epoch to decay learning rate')
+parser.add_argument('--phase', type=str, default='train',
+                    help='train or pretrain')
+
+# Model
+parser.add_argument('--downsample_type', type=str, default='desubpixel')
+parser.add_argument('--upsample_type', type=str, default='subpixel')
+parser.add_argument('--conv_type', type=str, default='default')
+parser.add_argument('--body_type', type=str, default='resnet')
+parser.add_argument('--n_feats', type=int, default=16,
+                    help='number of convolution feats')
+parser.add_argument('--n_blocks', type=int, default=20,
+                    help='number of residual block if body_type=resnet')
+parser.add_argument('--n_groups', type=int, default=0,
+                    help='number of residual group if body_type=res_in_res')
+parser.add_argument('--n_convs', type=int, default=0,
+                    help='number of conv layers if body_type=conv')
+parser.add_argument('--n_squeezes', type=int, default=0,
+                    help='number of squeeze blocks if body_type=squeeze')
+parser.add_argument('--pretrained_model', type=str, default='', 
+                    help='if specified, fine tune on pretrained model')
+
+# Loss
+parser.add_argument('--alpha_mse', type=float, default=1)
+parser.add_argument('--alpha_vgg', type=float, default=1e-4)
+parser.add_argument('--vgg_dir', type=str, default='vgg_pretrained/imagenet-vgg-verydeep-19.mat',
+                    help='vvg model for vgg loss')
+# Logging
+parser.add_argument('--checkpoint', type=str, default='checkpoint',
+                    help='save logs and models')
+parser.add_argument('--eval_every', type=int, default=20)
 args = parser.parse_args()
 
-###====================== HYPER-PARAMETERS ===========================###
-## Adam
-batch_size = args.batch_size
-lr_init = config.TRAIN.lr_init
-beta1 = config.TRAIN.beta1
-## adversarial learning (SRGAN)
-n_epoch = config.TRAIN.n_epoch
-lr_decay = config.TRAIN.lr_decay
-decay_every = config.TRAIN.decay_every
-checkpoint = args.checkpoint
+print('############################################################')
+print('# Image Super Resolution - PIRM2018 - TEAM_ALEX            #')
+print('# Implemented by Thang Vu, thangvubk@gmail.com             #')
+print('############################################################')
+print('')
+print('_____________YOUR SETTINGS_____________')
+for arg in vars(args):
+    print("%20s: %s" %(str(arg), str(getattr(args, arg))))
+print('')
 
 def train():
     ## create folders to save trained model
-    tl.files.exists_or_mkdir(checkpoint)
+    tl.files.exists_or_mkdir(args.checkpoint)
 
     ###====================== PRE-LOAD DATA ===========================###
     train_hr_npy = os.path.join(args.train_path, 'train_hr.npy')
@@ -57,11 +79,12 @@ def train():
     valid_lr_npy = os.path.join(args.valid_path, 'X{}_valid_lr.npy'.format(args.scale))
 
     if os.path.exists(train_hr_npy) and os.path.exists(valid_hr_npy) and os.path.exists(valid_lr_npy):
+        print('Loading data...')
         train_hr_imgs = np.load(train_hr_npy)
         valid_hr_imgs = np.load(valid_hr_npy)
         valid_lr_imgs = np.load(valid_lr_npy)
     else:
-        print('Data bins is not created. Creating data bins')
+        print('Data bins is not created. Creating data bins...')
         train_hr_img_list = sorted(tl.files.load_file_list(path=args.train_path, regx='.*.png', printable=False))
         valid_hr_img_list = sorted(tl.files.load_file_list(path=args.valid_path, regx='.*.png', printable=False))
         train_hr_imgs = np.array(tl.vis.read_images(train_hr_img_list, path=args.train_path, n_threads=32))
@@ -76,19 +99,22 @@ def train():
     t_lr = tf.placeholder('float32', [None, None, None, 3], name='t_lr')
     t_hr = tf.placeholder('float32', [None, None, None, 3], name='t_hr')
 
+    # some options are mutual exclusive, check model for detail
     opt = {
         'n_feats': args.n_feats,
         'n_blocks': args.n_blocks,
         'n_groups': args.n_groups,
         'n_convs': args.n_convs,
         'n_squeezes': args.n_squeezes,
-        'sample_type': args.sample_type,
+        'downsample_type': args.downsample_type,
+        'upsample_type': args.upsample_type,
         'conv_type': args.conv_type,
         'body_type': args.body_type,
         'scale': args.scale
     }
-
-    t_sr = SRGAN_g(t_lr, opt)
+    
+    print('Loading model...')
+    t_sr = FEQE(t_lr, opt)
 
     ## Load VGG net
     vgg_dir = args.vgg_dir
@@ -97,10 +123,11 @@ def train():
         return
 
     CONTENT_LAYER = 'relu5_4'
-    sr_vgg = vgg19(vgg_dir, preprocess(t_sr * 255))
-    hr_vgg = vgg19(vgg_dir, preprocess(t_hr * 255))
+    with tf.variable_scope('VGG'):
+        sr_vgg = vgg19(vgg_dir, preprocess(t_sr * 255))
+        hr_vgg = vgg19(vgg_dir, preprocess(t_hr * 255))
 
-    # For loop: Count number of parameters
+    # Count number of parameters
     total_parameters = 0
     for variable in tf.trainable_variables():
         variable_parameters = 1
@@ -109,22 +136,23 @@ def train():
         total_parameters += variable_parameters
     print("Total number of trainable parameters: %d" % total_parameters)
 
-    ####========================== DEFINE TRAIN OPS ==========================###
-    ## Define G loss
-    #l1_loss = args.alpha_l1*tl.cost.absolute_difference_error(t_sr, t_hr, is_mean=True)
-    l1_loss = args.alpha_l1*tl.cost.mean_squared_error(t_sr, t_hr, is_mean=True)
-    vgg_loss = args.alpha_vgg*tl.cost.mean_squared_error(sr_vgg[CONTENT_LAYER], hr_vgg[CONTENT_LAYER], is_mean=True) if args.alpha_vgg != 0 else tf.constant(0.0)
-    g_loss = vgg_loss  + l1_loss
+    ####========================== Loss function ==========================###
+    #mse_loss = args.alpha_mse*tl.cost.absolute_difference_error(t_sr, t_hr, is_mean=True)
+    mse_loss = args.alpha_mse*tl.cost.mean_squared_error(t_sr, t_hr, is_mean=True)
 
+    with tf.variable_scope('VGG_loss'):
+        vgg_loss = args.alpha_vgg*tl.cost.mean_squared_error(sr_vgg[CONTENT_LAYER], hr_vgg[CONTENT_LAYER], is_mean=True) if args.alpha_vgg != 0 else tf.constant(0.0)
+
+    g_loss = vgg_loss  + mse_loss
+
+    #==========================Training ops==================================
     g_vars = tl.layers.get_variables_with_name('Generator', True, True)
-
     with tf.variable_scope('learning_rate'):
-        lr_v = tf.Variable(lr_init, trainable=False)
-    ## Pretrain
-    g_optim = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(g_loss, var_list=g_vars)
+        lr_v = tf.Variable(args.learning_rate, trainable=False)
+    g_optim = tf.train.AdamOptimizer(lr_v).minimize(g_loss, var_list=g_vars)
 
 
-    #=============PSNR and SSIM================================================
+    #===========================PSNR and SSIM================================
     t_psnr = tf.image.psnr(t_sr, t_hr, max_val=1.0)
     t_ssim = tf.image.ssim_multiscale(t_sr, t_hr, max_val=1.0)
 
@@ -137,60 +165,51 @@ def train():
     else:
         global_saver = tf.train.Saver()
         if args.pretrained_model != '':
-            body_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'Generator'))
+            body_saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'Generator/body')) #TODO change to Generator/body
             body_saver.restore(sess, args.pretrained_model)
 
-    ###============================= LOAD VGG ===============================###
-
-
     ###=========================Tensorboard=============================###
-    writer = SummaryWriter(os.path.join(checkpoint, 'result'))
-    tf.summary.FileWriter(os.path.join(checkpoint, 'graph'), sess.graph)
+    writer = SummaryWriter(os.path.join(args.checkpoint, 'result'))
+    tf.summary.FileWriter(os.path.join(args.checkpoint, 'graph'), sess.graph)
     best_score, best_epoch = -1, -1
 
     ###========================= Training ====================###
-    for epoch in range(1, n_epoch + 1):
+    for epoch in range(1, args.n_epochs + 1):
         ## update learning rate
-        if epoch == 1:
-            sess.run(tf.assign(lr_v, lr_init))
-            log = " ** init lr: %f  decay_every_init: %d, lr_decay: %f (for GAN)" % (lr_init, decay_every, lr_decay)
-            print(log)
-        if epoch % decay_every == 0:
-            new_lr_decay = lr_decay**(epoch // decay_every)
-            sess.run(tf.assign(lr_v, lr_init * new_lr_decay))
-            log = " ** new learning rate: %f (for GAN)" % (lr_init * new_lr_decay)
+        if epoch % args.decay_every == 0:
+            new_lr_decay = 0.5**(epoch // args.decay_every)
+            sess.run(tf.assign(lr_v, args.learning_rate * new_lr_decay))
+            log = " ** new learning rate: %f " % (args.learning_rate * new_lr_decay)
             print(log)
 
         # ids to shuffle batches
         ids = np.random.permutation(len(train_hr_imgs))
 
         epoch_time = time.time()
-        num_batches = len(train_hr_imgs)//batch_size
+        num_batches = len(train_hr_imgs)//args.batch_size
         # running_loss = 0
-        total_vgg_loss, total_l1_loss, total_g_loss = 0, 0, 0
+        total_vgg_loss, total_mse_loss, total_g_loss = 0, 0, 0
+        running_loss = np.zeros(3)
 
         for i in tqdm(range(num_batches)):
-            hr = tl.prepro.threading_data(train_hr_imgs[ids[i*batch_size:(i+1)*batch_size]],
+            hr = tl.prepro.threading_data(train_hr_imgs[ids[i*args.batch_size:(i+1)*args.batch_size]],
                                           fn=crop_sub_imgs_fn, is_random=True)
             lr = tl.prepro.threading_data(hr, fn=downsample_fn, scale=args.scale)
             [lr, hr] = normalize([lr, hr])
 
             ## update G
-            errG, errL, errV, _ = sess.run([g_loss, l1_loss, vgg_loss, g_optim], {t_lr: lr, t_hr: hr})
+            errG, errL, errV, _ = sess.run([g_loss, mse_loss, vgg_loss, g_optim], {t_lr: lr, t_hr: hr})
 
-            total_g_loss += errG
-            total_l1_loss += errL
-            total_vgg_loss += errV
-
-        log = "[*] Epoch: [%2d/%2d], g_loss: %.6f, l1_loss: %.6f, vgg_loss: %.6f" % \
-              (epoch, n_epoch, total_g_loss/num_batches, total_l1_loss/num_batches,
-               total_vgg_loss/num_batches)
+            running_loss += [errG, errL, errV]
+        
+        avr_loss = running_loss/num_batches
+        log = "[*] Epoch: [%2d/%2d], g_loss: %.6f, mse_loss: %.6f, vgg_loss: %.6f" % \
+              (epoch, args.n_epochs, avr_loss[0], avr_loss[1], avr_loss[2])
         print(log)
 
-        writer.add_scalar('G_total_Loss', total_g_loss/num_batches, epoch)
-        writer.add_scalar('MSE_Loss', total_l1_loss / num_batches, epoch)
-        writer.add_scalar('VGG_Loss', total_vgg_loss / num_batches, epoch)
-
+        writer.add_scalar('G_total_Loss', avr_loss[0], epoch)
+        writer.add_scalar('MSE_Loss', avr_loss[1], epoch)
+        writer.add_scalar('VGG_Loss', avr_loss[2], epoch)
 
         #=============Valdating==================#
         running_loss = 0
@@ -211,11 +230,11 @@ def train():
 
                 psnr, ssim,  sr_ex = sess.run([t_psnr, t_ssim, t_sr], {t_lr: lr_ex, t_hr: hr_ex})
                 sr = np.squeeze(sr_ex)
-
-                #[lr, sr, hr] = restore([lr, sr, hr])
+                
+                #pdb.set_trace()
                 update_tensorboard(epoch, writer, i, lr, sr, hr)
 
-                val_psnr += psnr #compute_PSNR(hr, sr)
+                val_psnr += psnr
                 val_ssim += ssim
                 loss = 0
                 running_loss += loss
@@ -223,7 +242,7 @@ def train():
                 # score referred to https://github.com/aiff22/ai-challenge
                 score += (psnr-26.5) + (ssim-0.94)*100
 
-
+            #global_saver.save(sess, os.path.join(args.checkpoint, 'model_{}.ckpt'.format(epoch)))
 
             val_psnr = val_psnr/len(valid_hr_imgs)
             val_ssim = val_ssim/len(valid_hr_imgs)
@@ -235,10 +254,10 @@ def train():
                 print('Saving new best model')
 
                 if args.phase == 'pretrain':
-                    body_saver.save(sess, os.path.join(checkpoint, 'body.ckpt'))
+                    body_saver.save(sess, os.path.join(args.checkpoint, 'body.ckpt'))
                 else:
-                    global_saver.save(sess, os.path.join(checkpoint, 'model.ckpt'))
-            print('Validate score: %.4fdB. Best: %.4fdB at epoch %d' %(score, best_score, best_epoch))
+                    global_saver.save(sess, os.path.join(args.checkpoint, 'model.ckpt'))
+            print('Validate score: %.4f. Best: %.4f at epoch %d' %(score, best_score, best_epoch))
             writer.add_scalar('Validate PSNR', val_psnr, epoch)
             writer.add_scalar('Validate SSIM', val_ssim, epoch)
             writer.add_scalar('Validate score', score, epoch)
